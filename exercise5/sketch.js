610b546e842d676284c2a0bae2493c3305280508
Aha2002-Tech <andeifhassan@gmail.com>
2025-10-23 21:19:50 +0100

    reputation of _charecter

---
+++ exercise5/sketch.js
@@ -0,0 +1,30 @@
+function setup (){
+
+createCanvas(800,800);
+
+
+}
+function draw (){
+
+background(255);
+
+
+
+
+
+//head 
+rectMode(CENTER);
+ fill(20,255,20);
+ ellipse(400,300,100,150);
+
+
+// eyes 1
+rectMode(CENTER);
+fill(255);
+ ellipse(380,300,10,10);
+//eye2
+rectMode(CENTER);
+fill(255);
+ ellipse(400,300,10,10);
+
+}
